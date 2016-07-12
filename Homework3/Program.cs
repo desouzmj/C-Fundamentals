8d608147cf2d16cb5f167ecb3f32fe52d9b8c9d8
desouzmj <desouzmj@gmail.com>
2016-07-11 21:02:53 -0700

    Homework 3

---
+++ Homework3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int leftover;
+            int secpermin = 60;
+            int secperhour = secpermin * secpermin;
+            int secperday = secperhour*24;
+            Console.Write("Enter number of seconds: ");
+            string strseconds = Console.ReadLine();
+            int seconds = int.Parse(strseconds);
+            int days = Math.DivRem(seconds, secperday, out leftover);
+            int hours = Math.DivRem(leftover, secperhour, out leftover);
+            int minutes = Math.DivRem(leftover, secpermin, out leftover);
+            Console.Write("{0} days, {1} hours, {2} minutes, {3} seconds", days, hours, minutes, leftover);
+            Console.ReadLine();
+        }
+    }
+}
