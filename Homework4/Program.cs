45d21dc2b64587b673cf1fb640b4c25f8b0a8c5c
desouzmj <desouzmj@gmail.com>
2016-07-18 19:28:56 -0700

    Homework4

---
+++ Homework4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int factorial =1;
+            Console.WriteLine("Enter factorial to calculate: ");
+            string x = Console.ReadLine();
+            int n = int.Parse(x);
+            int i;
+
+            for (i=n; i>1; i=i-1)
+            {
+                factorial = factorial * i;
+            }
+            Console.WriteLine("Factorial: {0}", factorial);
+            Console.ReadLine();
+        }
+    }
+}
